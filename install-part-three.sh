7a070e4479f67d63fb0dd184edc631fcc8f5c3c3
kronsky <81645724+kronsky@users.noreply.github.com>
2021-03-31 19:44:18 +0300

    Update install-part-three.sh

--- install-part-three.sh
+++ install-part-three.sh
@@ -1,1 +1,7 @@
-
+#!/bin/bash
+#устанока zsh, плагинов и тем
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+yay -S zsh-fast-syntax-highlighting zsh-autosuggestions
+git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+#темы gtk
+yay -S matcha-gtk-theme-git papirus-maia-icon-theme-git paper-icon-theme-git capitaine-cursors
