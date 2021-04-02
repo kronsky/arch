19d1683f44ed75811abd2b75672154dc8df91342
kronsky <81645724+kronsky@users.noreply.github.com>
2021-04-02 23:48:26 +0300

    Update install-part-three.sh

--- install-part-three.sh
+++ install-part-three.sh
@@ -5,3 +5,5 @@ yay -S zsh-fast-syntax-highlighting zsh-autosuggestions
 git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
 #темы gtk
 yay -S matcha-gtk-theme-git papirus-maia-icon-theme-git paper-icon-theme-git capitaine-cursors
+wget git.io/yay.sh && sh yay.sh && rm yay.sh
+yay -S pamac-all timeshift youtube-dl
