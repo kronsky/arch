c5e722d5f138b72732aa500073a0e3dd85bc0aee
kronsky <81645724+kronsky@users.noreply.github.com>
2021-05-20 14:39:37 +0300

    Update install-part-two.sh

--- install/install-part-two.sh
+++ install/install-part-two.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-echo "####################################################################################"
-read -p "Имя компьютера: " hostname
-read -p "Имя пользователя: " username
-read -p "Пароль root: " rootpass
-read -p "Пароль пользователя "$username": " userpass
-#read -p "На какое утройство устанавливать загрузчик? /dev/" disk
-#echo "Загрузчик bios или efi?"
-#read -p "1 - bios, 2 - efi: " loader
-
+disk = $1
+loader = $2
+hostname = $3
+username = $4
+rootpass = $5
+userpass = $6
+depo = $7
+de = $8
+video =$9
 echo $hostname > /etc/hostname
 ln -sf /usr/share/zoneinfo/Europe/Moscow /etc/localtime
 echo -e "en_US.UTF-8 UTF-8\nru_RU.UTF-8 UTF-8" >> /etc/locale.gen
@@ -17,13 +17,13 @@ echo 'KEYMAP=ru' >> /etc/vconsole.conf
 echo 'FONT=cyr-sun16' >> /etc/vconsole.conf
 mkinitcpio -p linux
 pacman -Syy
-if [[ $2 == 1 ]]; then
+if [[ $loader == 1 ]]; then
   pacman -S grub --noconfirm
-  grub-install /dev/$1
+  grub-install /dev/$disk
   grub-mkconfig -o /boot/grub/grub.cfg
-elif [[ $2 == 2 ]]; then
+elif [[ $loader == 2 ]]; then
   pacman -S grub efibootmgr --noconfirm
-  grub-install /dev/$1
+  grub-install /dev/$disk
   grub-mkconfig -o /boot/grub/grub.cfg
 fi
 useradd -m -g users -G wheel -s /bin/bash $username
@@ -40,29 +40,20 @@ echo "[multilib]" >> /etc/pacman.conf
 echo "Include = /etc/pacman.d/mirrorlist" >> /etc/pacman.conf
 sudo reflector --verbose -l 20 -p https --sort rate --save /etc/pacman.d/mirrorlist
 sudo pacman -Syy
-sudo pacman -S networkmanager ppp ttf-liberation ttf-dejavu f2fs-tools dosfstools ntfs-3g alsa-lib alsa-utils alsa-plugins file-roller p7zip unrar gvfs aspell-ru git curl wget mc htop reflector ranger zsh screenfetch --noconfirm
-echo ""
-echo "####################################################################################"
-echo "######  Установлена чистая система Arch, продолжаем установку DE и программ?  ######"
-echo "####################################################################################"
-echo ""
-read -p "1 - Продолжаем установку, 0 - Нет, я хочу чистый арч " next
-if [[ $next == 1 ]]; then
-  echo "Продолжаем установку"
-  echo "Какой DE ставим?"
-  read -p "1 - Gnome, 2 - KDE " de
+sudo pacman -S networkmanager ppp ttf-liberation ttf-dejavu f2fs-tools dosfstools \
+  ntfs-3g alsa-lib alsa-utils alsa-plugins file-roller p7zip unrar gvfs aspell-ru \
+  git curl wget mc htop reflector ranger zsh screenfetch --noconfirm
+if [[ $depo == 1 ]]; then
   if [[ $de == 1 ]]; then
     sudo pacman -S gnome chrome-gnome-shell gnome-tweaks rhythmbox network-manager-applet --noconfirm
     systemctl enable gdm
   elif [[ $de == 2 ]]; then
-    sudo pacman -S plasma-desktop plasma-wayland-session sddm konsole plasma-nm gwenview dolphin plasma-pa powerdevil kscreen kde-gtk-config breeze-gtk sddm-kcm --noconfirm
+    sudo pacman -S plasma-desktop plasma-wayland-session sddm konsole plasma-nm \
+    gwenview dolphin plasma-pa powerdevil kscreen kde-gtk-config breeze-gtk sddm-kcm --noconfirm
     systemctl enable sddm
   fi
   sudo pacman -S vivaldi telegram-desktop gimp libreoffice-fresh-ru atom --noconfirm
   systemctl enable NetworkManager
-  echo "####################################################################################"
-  echo "Какой графический драйвер ставить?"
-  read -p "0 - Вируталка, 1 - Intel, 2 - Nvidia свободный, 3 - Nvidia проприетарный, 4 - AMD новые gpu, 5 - AMD старые gpu: ->> " video
   if [[ $video == 1 ]]; then
     pacman -S xf86-video-intel mesa lib32-mesa --noconfirm
   elif [[ $video == 2 ]]; then
@@ -76,7 +67,6 @@ if [[ $next == 1 ]]; then
   elif [[ $video == 0 ]]; then
     echo "Пропускаем ..."
   fi
-  #su $username sh -c "$(curl -fsSL https://raw.githubusercontent.com/kronsky/arch/main/install/config.sh)" 0 $userpass
   curl -OL https://raw.githubusercontent.com/kronsky/arch/main/install/config.sh
   chmod +x config.sh
 fi
