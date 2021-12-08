9848c0df36e212f1884eb7b51faa5efd818f5bb8
kronsky <81645724+kronsky@users.noreply.github.com>
2021-12-08 18:44:27 +0300

    Update config.sh

--- install/config.sh
+++ install/config.sh
@@ -2,12 +2,12 @@
 
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 sudo git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+
 git clone https://github.com/kronsky/arch.git
 cp -f ~/arch/config/.alias_zsh ~/
-cp -f ~/arch/config/.p10k.zsh ~/
 cp -f ~/arch/config/.zshrc ~/
 
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
-yay -S pamac-all timeshift youtube-dl zsh-fast-syntax-highlighting zsh-autosuggestions --noconfirm
+yay -S youtube-dl zsh-fast-syntax-highlighting zsh-autosuggestions --noconfirm
